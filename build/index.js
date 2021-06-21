0f8bdbc676ed08caf80e3ec593653c3b24174479
Nelson Chan <chakhong.chan@aecom.com>
2021-06-21 14:33:00 +0800

    Fix: Use production build

--- build/index.js
+++ build/index.js
@@ -1,16 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-import { p as identity } from './main2.js';
-
-function fade(node, { delay = 0, duration = 400, easing = identity } = {}) {
-    const o = +getComputedStyle(node).opacity;
-    return {
-        delay,
-        duration,
-        easing,
-        css: t => `opacity: ${t * o}`
-    };
-}
-
-export { fade as f };
+import{n as t}from"./main2.js";function a(a,{delay:n=0,duration:o=400,easing:e=t}={}){const i=+getComputedStyle(a).opacity;return{delay:n,duration:o,easing:e,css:t=>"opacity: "+t*i}}export{a as f};
 //# sourceMappingURL=index.js.map
--- build/main.js
+++ build/main.js
@@ -1,4 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { q as default } from './main2.js';
+export{o as default}from"./main2.js";
 //# sourceMappingURL=main.js.map
