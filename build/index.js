517a841360b7329270280788993d57d8232452df
Nelson Chan <chakhong.chan@aecom.com>
2021-09-08 17:47:13 +0800

    Pages: Update build

--- build/index.js
+++ build/index.js
@@ -1,2 +1,16 @@
-import{o as t}from"./main2.js";function a(a,{delay:o=0,duration:e=400,easing:i=t}={}){const n=+getComputedStyle(a).opacity;return{delay:o,duration:e,easing:i,css:t=>"opacity: "+t*n}}export{a as f};
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+import { F as identity } from './main2.js';
+
+function fade(node, { delay = 0, duration = 400, easing = identity } = {}) {
+    const o = +getComputedStyle(node).opacity;
+    return {
+        delay,
+        duration,
+        easing,
+        css: t => `opacity: ${t * o}`
+    };
+}
+
+export { fade as f };
 //# sourceMappingURL=index.js.map
--- build/main.js
+++ build/main.js
@@ -1,2 +1,2 @@
-export{q as default}from"./main2.js";
+export{L as default}from"./main2.js";
 //# sourceMappingURL=main.js.map
